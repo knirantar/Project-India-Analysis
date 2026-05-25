3b05c0ba7a6f369dfe9f0ec40417c5ea2e0d8284
knirantar <knirantar@gmail.com>
2026-05-25 22:35:32 +0530

    Fix Vercel deployment output

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || ".next",
-  output: "standalone"
+  distDir: process.env.NEXT_DIST_DIR || ".next"
 };
 
 export default nextConfig;
